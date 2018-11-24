e6487714c9664a66fd0e9bba07fa54e1cc6587fa
Rodolfo Oliveira <oliveira.rodolfo87@gmail.com>
2018-11-24 12:02:15 +0000

    get the minmax common intervals of given tracks

--- scripts/utils.py
+++ scripts/utils.py
@@ -7,3 +7,20 @@ def las2df(fname):
     df = w.df()
     return df
 
+
+def minmax_idx(logs_df, tracks):    
+    imin = 0
+    imax = logs_df.shape[0]-1
+
+    for itrack in tracks:
+        vals = logs_df[itrack].values
+        idx = np.where(np.isnan(vals) == False)[0]
+        imin = max(imin, idx.min())
+        imax = min(imax, idx.max())
+
+    return imin, imax
+
+
+def minmax_depth(logs_df, tracks):
+    imin, imax = minmax_idx(logs_df, tracks)
+    return logs_df.index[imin], logs_df.index[imax]
